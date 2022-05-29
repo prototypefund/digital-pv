1610c2b473ad1aaae7bd794164f39d336bb90787
Marius Jeskulke <marius@deyan7.de>
2022-05-29 08:35:07 +0200

    Add missing routes constants

--- lib/navigation/routes.dart
+++ lib/navigation/routes.dart
@@ -2,8 +2,13 @@ class Routes {
   Routes._();
 
   static const welcome = '/';
-  static const positiveAspects = '/current-situation/positive-aspects';
-  static const negativeAspects = '/current-situation/negative-aspects';
-  static const evaluateCurrentAspects = '/current-situation/evaluate';
-  static const generalTreatmentObjective = '/treatment-objective';
+  static const positiveAspects = '/patient-directive/current-situation/positive-aspects';
+  static const negativeAspects = '/patient-directive/current-situation/negative-aspects';
+  static const evaluateCurrentAspects = '/patient-directive/current-situation/evaluate';
+  static const generalTreatmentObjective = '/patient-directive/treatment-objective';
+  static const treatmentActivities = '/patient-directive/treatment-objective/treatment-activities';
+  static const futureSituations = '/patient-directive/future-situations';
+  static const trustedThirdParty = '/patient-directive/trusted-third-party';
+  static const generalInformationAboutPatientDirective = '/patient-directive/general-information';
+  static const personalDetails = '/patient-directive/personal-details';
 }
