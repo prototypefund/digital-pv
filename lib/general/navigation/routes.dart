62f86ee32dca963f1032a5b6000b8cb89624047b
Marius Jeskulke <marius@deyan7.de>
2022-06-03 16:19:05 +0200

    Using constant to build patient directive routes

--- lib/general/navigation/routes.dart
+++ lib/general/navigation/routes.dart
@@ -1,14 +1,16 @@
 class Routes {
   Routes._();
 
+  static const patientDirective = 'patient-directive/';
+
   static const welcome = '/';
-  static const positiveAspects = '/patient-directive/current-situation/positive-aspects';
-  static const negativeAspects = '/patient-directive/current-situation/negative-aspects';
-  static const evaluateCurrentAspects = '/patient-directive/current-situation/evaluate';
-  static const generalTreatmentObjective = '/patient-directive/treatment-objective';
-  static const treatmentActivities = '/patient-directive/treatment-objective/treatment-activities';
-  static const futureSituations = '/patient-directive/future-situations';
-  static const trustedThirdParty = '/patient-directive/trusted-third-party';
-  static const generalInformationAboutPatientDirective = '/patient-directive/general-information';
-  static const personalDetails = '/patient-directive/personal-details';
+  static const positiveAspects = '/${patientDirective}current-situation/positive-aspects';
+  static const negativeAspects = '/${patientDirective}current-situation/negative-aspects';
+  static const evaluateCurrentAspects = '/${patientDirective}current-situation/evaluate';
+  static const generalTreatmentObjective = '/${patientDirective}treatment-objective';
+  static const treatmentActivities = '/${patientDirective}treatment-objective/treatment-activities';
+  static const futureSituations = '/${patientDirective}future-situations';
+  static const trustedThirdParty = '/${patientDirective}trusted-third-party';
+  static const generalInformationAboutPatientDirective = '/${patientDirective}general-information';
+  static const personalDetails = '/${patientDirective}personal-details';
 }
