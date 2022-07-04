457ceb28469d176243178f027b059e91153528f3
Marius Jeskulke <marius@deyan7.de>
2022-07-04 16:11:03 +0200

    Fix test case by adjusting assets paths

--- lib/general/themes/images.dart
+++ lib/general/themes/images.dart
@@ -1,18 +1,3 @@
-import 'package:flutter/foundation.dart';
-
 mixin Images {
-  static const String _backgroundDecorationPath = 'images/background.svg';
-
-  static String backgroundDecorationPath() => _buildAssetPathBasedOnPlattform(rawPath: _backgroundDecorationPath);
-
-  /// See https://stackoverflow.com/questions/61301598/how-can-i-display-asset-images-on-flutter-web for the reason this exists
-  ///
-  /// tldr: Flutter Web needs an additional '/assets' prefix for its asset paths. Widget tests ail without this
-  static String _buildAssetPathBasedOnPlattform({required String rawPath}) {
-    if (kIsWeb) {
-      return 'assets/$rawPath';
-    } else {
-      return rawPath;
-    }
-  }
+  static String backgroundDecorationPath() => 'assets/images/background.svg';
 }
