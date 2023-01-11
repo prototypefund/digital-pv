0af5962029c50147734c155e7040224547c907bf
Marius Jeskulke <marius@deyan7.de>
2023-01-11 16:06:38 +0100

    add all elements of evaluate current situation screen

--- lib/use_cases/evaluate_current_aspects/evaluate_current_aspects_view.dart
+++ lib/use_cases/evaluate_current_aspects/evaluate_current_aspects_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pd_app/general/creation_process_navigation/creation_process_navigation.dart';
+import 'package:pd_app/general/themes/constraints.dart';
 import 'package:pd_app/general/themes/paddings.dart';
 import 'package:pd_app/general/view_components/aspect_visualization/aspect_visualization.dart';
 import 'package:pd_app/general/view_components/aspect_visualization/aspect_visualization_view_model.dart';
@@ -18,22 +19,51 @@ class EvaluateCurrentAspects extends StatelessWidget {
   Widget build(BuildContext context) {
     final EvaluateCurrentAspectsViewModel _viewModel = context.watch();
     return CreationProcessNavigation<EvaluateCurrentAspectsViewModel>(
-        widget: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-      Padding(
-        padding: Paddings.headlinePadding,
-        child: Text(_viewModel.headline, style: Theme.of(context).textTheme.headlineLarge),
-      ),
-      Padding(
-          padding: Paddings.headlineExplanationPadding,
-          child: Text(
-            _viewModel.summary,
-            style: Theme.of(context).textTheme.bodyLarge,
-            textAlign: TextAlign.center,
-          )),
-      const SizedBox(
-        height: 40,
-      ),
-      ChangeNotifierProvider(create: (_) => AspectVisualizationViewModel(), child: AspectVisualization()),
-    ]));
+        widget: SizedBox(
+      width: double.infinity,
+      child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+        Padding(
+          padding: Paddings.headlinePadding,
+          child: Text(_viewModel.headline, style: Theme.of(context).textTheme.headlineLarge),
+        ),
+        const SizedBox(
+          height: 24,
+        ),
+        Text(
+          _viewModel.summary,
+          style: Theme.of(context).textTheme.bodyLarge,
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(
+          height: 40,
+        ),
+        ConstrainedBox(
+            constraints: Constraints.aspectVisualizationConstraints,
+            child: ChangeNotifierProvider(create: (_) => AspectVisualizationViewModel(), child: AspectVisualization())),
+        const SizedBox(
+          height: 24,
+        ),
+        Text(
+          _viewModel.confirmEvaluationQuestion,
+          style: Theme.of(context).textTheme.bodyLarge,
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(
+          height: 24,
+        ),
+        ElevatedButton(
+          onPressed: () => _viewModel.onConfirmPressed(context),
+          child: Text(_viewModel.confirmEvaluation),
+        ),
+        const SizedBox(
+          height: 24,
+        ),
+        Text(
+          _viewModel.changeAspectsIfNoMatchCallToAction,
+          style: Theme.of(context).textTheme.bodyMedium,
+          textAlign: TextAlign.center,
+        )
+      ]),
+    ));
   }
 }
