d7dcb08829bd659ef8b6e49c93de4abb5680982e
Marius Jeskulke <marius@deyan7.de>
2023-01-18 11:04:23 +0100

    restrict weight of aspects to valid range instead of throwing exception

--- lib/general/model/weight.dart
+++ lib/general/model/weight.dart
@@ -1,16 +1,15 @@
+import 'dart:math';
+
 class Weight {
   /// weight between 0 (no importance) and 1 (maximum importance)
   final double value;
 
-  Weight({required this.value}) {
-    _validateWeightInRange(value);
-  }
+  static const double valueUpperBound = 1;
+  static const double valueLowerBound = 0;
 
-  void _validateWeightInRange(double weightToCheck) {
-    if (value < 0 || weightToCheck > 1) {
-      throw InvalidWeightException();
-    }
+  Weight({required double value}) : value = _restrictValueToValidBounds(value);
+
+  static double _restrictValueToValidBounds(double valueToCheck) {
+    return max(min(valueToCheck, valueUpperBound), valueLowerBound);
   }
 }
-
-class InvalidWeightException implements Exception {}
