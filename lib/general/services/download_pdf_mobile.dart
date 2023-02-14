17735910650139a5a680d5a081d46b1570215c78
Torben Kreuder <torben.kreuder@gmail.com>
2023-02-14 09:15:12 +0100

    Adjust signature so that pdf is saved in files

--- lib/general/services/download_pdf_mobile.dart
+++ lib/general/services/download_pdf_mobile.dart
@@ -5,6 +5,8 @@ import 'package:flutter/foundation.dart';
 import 'package:share_plus/share_plus.dart';
 
 void downloadPdf(Uint8List bytes, String filename, String path) {
-  unawaited(Share.shareXFiles([XFile(path, name: filename, bytes: bytes)], text: filename));
+  unawaited(Share.shareXFiles([XFile.fromData(bytes, mimeType: 'application/pdf', name: filename)],
+      text: filename, subject: filename));
+
   unawaited(File(path).delete());
 }
