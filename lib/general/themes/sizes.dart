c61c3861730d962491436a8539398a718a022c45
Marius Jeskulke <marius@deyan7.de>
2023-03-30 09:17:42 +0200

    increase size of aspects without treatment options

--- lib/general/themes/sizes.dart
+++ lib/general/themes/sizes.dart
@@ -3,5 +3,5 @@ mixin Sizes {
   static const double elevatedButtonLabelIconDistance = 12;
   static const double checkboxGroupDistance = 16;
   static const double aspectListItemWithTreatmentOptionsHeight = 470;
-  static const double aspectListItemWithoutTreatmentOptionsHeight = 180;
+  static const double aspectListItemWithoutTreatmentOptionsHeight = 190;
 }
